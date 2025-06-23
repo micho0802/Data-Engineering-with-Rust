d4965686603e23b8603605ed5e7bc906651f359e
Long Dang <117198788+micho0802@users.noreply.github.com>
2025-06-23 21:37:06 +0000

    External lab, create a fruit salad 4

--- vector-fruit-salad/src/main.rs
+++ vector-fruit-salad/src/main.rs
@@ -8,18 +8,30 @@ useful data structures in Rust. A vector is represented using the Vec<T> type.
 // such as shuffling.
 use rand::seq::SliceRandom; // rand is a random number generation library in Rust
 use rand::thread_rng;
+use std::io;
+
+fn input_fruit() -> String{
+    println!("Please enter a fruit name:");
+    let mut user_input: String = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
+    user_input.trim().to_string() // trim() removes whitespace from the beginning and end of the string
+}
+
 
 fn main() {
-    let mut fruit = vec![
-        "Orange",
-        "Fig",
-        "Pomegranate",
-        "Cherry",
-        "Apple",
-        "Pear",
-        "Peach",
+    let mut user_input = input_fruit();
+    let mut fruit: Vec<String> = vec![
+        "Orange".to_string(),
+        "Fig".to_string(),
+        "Pomegranate".to_string(),
+        "Cherry".to_string(),
+        "Apple".to_string(),
+        "Pear".to_string(),
+        "Peach".to_string(),
     ];
-
+    fruit.push(user_input);
     // Scramble (shuffle) the fruit
     let mut rng = thread_rng();
     fruit.shuffle(&mut rng);
