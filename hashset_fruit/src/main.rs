83a5ab94e3d23d8a055ffd5b7915e9617381f8c8
Long Dang <117198788+micho0802@users.noreply.github.com>
2025-06-26 22:27:33 +0000

    Question 1

--- hashset_fruit/src/main.rs
+++ hashset_fruit/src/main.rs
@@ -2,10 +2,21 @@ use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::collections::HashSet;
 use std::io;
+use rand::prelude::IndexedRandom;
 
 fn ask_user_number() -> u8 {
     println!("How many number of random fruit?");
-    
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    match input.trim().parse::<u8>() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input, defaulting to 9.");
+            9
+        }
+    }
 }
 
 fn generate_fruit() -> &'static str {
@@ -25,8 +36,9 @@ fn generate_fruit() -> &'static str {
 
 fn main() {
     let mut fruit_set = HashSet::new();
-    println!("Generating 100 random fruits...");
-    for _ in 0..100 {
+    let number_of_fruits = ask_user_number();
+    println!("Generating {} random fruits...", number_of_fruits);
+    for _ in 0..number_of_fruits {
         fruit_set.insert(generate_fruit());
     }
 
